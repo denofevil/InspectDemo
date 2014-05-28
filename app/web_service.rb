8cec63a81fb148a79f16bea3bca5dafc305afd61
Dennis Ushakov <dennis.ushakov@gmail.com>
2014-05-28 07:03:51 -0700

    more words

--- app/web_service.rb
+++ app/web_service.rb
@@ -1,5 +1,6 @@
 class WebService
-  KNOWN_WORDS = ["hello", "inspect", ""]
+  KNOWN_WORDS = ["hello", "inspect", "motion", "ruby", "rubymotion", "jetbrains", "rubymine", ""]
+
   # @param [String] text
   # @return [Array] array of words with spelling error
   def self.validateText(text)
